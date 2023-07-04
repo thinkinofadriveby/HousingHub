5cf819e9007a8b6ea5a85b0aba3249a3e9c41f1c
thinkinofadriveby <nazaryuras228@gmail.com>
2023-07-04 20:14:13 +0300

    Added some more cards of houses and its design

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component(
@@ -9,12 +9,7 @@ import { Component } from "@angular/core";
     }
 )
 export class PropertyCardComponent {
-    Property: any = {
-        "Id": 1,
-        "Name":"La Vida Loca House",
-        "Type": "House",
-        "Price":12000
-    }
 
+    @Input() property : any
 
 }
