502a86be53a34fb66fbdfa83823b5c6f96442581
thinkinofadriveby <nazaryuras228@gmail.com>
2023-07-18 21:15:28 +0300

    Fixed rewriting the value of a house card instead of adding a new one

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -27,12 +27,6 @@ export class PropertyListComponent implements OnInit {
         data => {
           this.properties = data;
           console.log(data);
-
-          const newProperty = JSON.parse(localStorage.getItem('newProp') || '""');
-
-          if (newProperty.SellRent === this.SellRent) {
-            this.properties = [newProperty, ...this.properties];
-          }
         }, error => {
           console.log('httperror:');
           console.log(error);
